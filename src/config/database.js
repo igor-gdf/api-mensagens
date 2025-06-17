cde1a033a582de2ddfdcb0c99545cf8cd46bb196
igorDeveloper <138522037+igor-gdf@users.noreply.github.com>
2025-06-17 17:38:11 +0000

    Migrando de SQLite para PostgreSQL

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,18 @@
 // database.js
-const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-});
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
-
